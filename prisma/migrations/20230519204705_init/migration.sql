e7de6b12aa713154ff36bbe1cb0a9a61ec92c260
Sosisochka1003 <91669317+Sosisochka1003@users.noreply.github.com>
2023-06-07 20:13:28 +0300

    klema

--- prisma/migrations/20230519204705_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `TimeSend` on the `messages` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE `messages` DROP COLUMN `TimeSend`,
-    ADD COLUMN `TimeSend` DATETIME(3) NOT NULL;
